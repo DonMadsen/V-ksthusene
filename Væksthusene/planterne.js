2a96f2960f37db68f06faad0daaeb048f6ff1ece
DonMadsen <70579667+DonMadsen@users.noreply.github.com>
2020-11-17 13:23:30 +0100

    Add files via upload

---
+++ Væksthusene/planterne.js
@@ -0,0 +1,11 @@
+function toggleElementVisibility(idOfElement) {
+
+    let elementToToggle = document.getElementById(idOfElement);
+
+    if(elementToToggle.style.display === 'block') {
+        elementToToggle.style.display = 'none';
+    }
+    else {
+        elementToToggle.style.display = 'block';
+    }
+}
